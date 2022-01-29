ddb2ff877f1e74778b6d65237f5f8bab122d7b1f
DaianeBonny <98478727+DaianeBonny@users.noreply.github.com>
2022-01-29 15:24:03 -0300

    fist commit

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,18 @@
+import br.com.dio.Gato;
+
+public class Primeiroprograma {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello Word!" + (a+b));
+       Gato gato_ = new Gato();
+
+        boolean gato;
+        System.out.println(Gato);
+
+
+
+
+    }
+
+}
